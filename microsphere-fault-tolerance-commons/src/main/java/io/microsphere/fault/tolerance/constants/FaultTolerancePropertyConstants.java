24852f5a386a9d4773b69e0d164ec3f59caa56c3
小马哥（mercyblitz） <mercyblitz@gmail.com>
2023-10-21 11:48:13 +0800

    Add microsphere-fault-tolerance-commons

---
+++ microsphere-fault-tolerance-commons/src/main/java/io/microsphere/fault/tolerance/constants/FaultTolerancePropertyConstants.java
@@ -0,0 +1,18 @@
+package io.microsphere.fault.tolerance.constants;
+
+import io.microsphere.constants.PropertyConstants;
+
+/**
+ * Fault-Tolerance Property Constants
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @since 1.0.0
+ */
+public interface FaultTolerancePropertyConstants {
+
+    /**
+     * Property name prefix
+     */
+    String FAULT_TOLERANCE_PROPERTY_NAME_PREFIX = PropertyConstants.MICROSPHERE_PROPERTY_NAME_PREFIX + "fault-tolerance.";
+
+}
